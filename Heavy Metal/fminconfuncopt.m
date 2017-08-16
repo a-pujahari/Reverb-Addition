019c8b01b4cd81702e043762149985a7f2ef9afe
a-pujahari <31046307+a-pujahari@users.noreply.github.com>
2017-08-16 11:52:20 +0200

    Add files via upload

---
+++ Heavy Metal/fminconfuncopt.m
@@ -0,0 +1,10 @@
+%% Mathematics of Mixing Optimization
+clear vars;
+f = @MinConTest_ult;
+G0=zeros(10,1);
+lb=zeros(10,1);
+ub=ones(10,1);
+
+G = fmincon(f,G0,[],[],[],[],lb,ub);
+save('Gmincon_ult.mat','G');
+
